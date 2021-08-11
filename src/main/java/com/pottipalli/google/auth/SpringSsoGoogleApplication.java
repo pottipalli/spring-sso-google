4d35ec459676166c8993435bf2bf7ffbc50c0a7f
sreenivas <sreenivas.04570@gmail.com>
2021-08-11 08:01:40 +0530

    first commit

---
+++ src/main/java/com/pottipalli/google/auth/SpringSsoGoogleApplication.java
@@ -0,0 +1,30 @@
+package com.pottipalli.google.auth;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class SpringSsoGoogleApplication {
+
+	@GetMapping
+	public String welcome() {
+		return "Welcome to Google...!";
+	}
+
+	@GetMapping("/user")
+	public Principal user(Principal principal) {
+		System.out.println(principal.getName());
+		return principal;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSsoGoogleApplication.class, args);
+	}
+
+}
